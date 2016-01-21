9b3315a86b9c7ba5bbb8dbe5dde4e7be2365b0af
wangfeng <wngfng@sina.cn>
2016-01-21 20:28:42 +0800

    Initial Commit

---
+++ 作业/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  作业
+//
+//  Created by qingyun on 16/1/21.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 作业/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  作业
+//
+//  Created by qingyun on 16/1/21.
+//  Copyright © 2016年 qingyun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
